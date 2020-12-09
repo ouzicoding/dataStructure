f10cb45d0ba2ce0ba9c35c2d90f73cbfa60aa2bb
ouhao <ouzi7997@163.com>
2020-12-09 17:31:38 +0800

    递归

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+function printN ($N)
+{
+    for($i=1;$i<=$N;$i++){
+        printf("%d\n",$i);
+    }
+    return;
+}
+printN(10000000);
+
+// 比上面快十倍 占用空间大
+function printN1 ($N)
+{
+    if ($N) {
+        printN1($N-1);
+        printf("%d\n",$N);
+    }
+    return;
+}
+
+printN1(10000000);
+
+
+
